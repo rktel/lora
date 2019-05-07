fa1861224f4013527c312d368d514da040dd43ff
rktel <rakinakuy@hotmail.com>
2019-05-07 15:58:23 -0500

    Anti Percy 3

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const server = net.createServer((c) => {
   });
   c.on('data', (data) => {
     console.log('Received:', data.toString());
+    const ParserData = Parser(data)
+    if(ParserData.VALIDATOR){
+      c.send(ParserData.ACK)
+    }
   })
   c.on('error', (...arg) => {
     console.log('Client Error:', ...arg)
@@ -23,3 +27,16 @@ server.on('error', (err) => {
 server.listen(3001, () => {
   console.log('server bound, port 3001');
 });
+
+function Parser(data) {
+  if (data.contains('+RESP')) {
+    let SplitData = data.split(',')
+    const ACK = '+SACK:' + SplitData[SplitData.length - 1]
+    return {
+      ACK,
+      VALIDATOR: true
+    }
+  } else {
+    return { VALIDATOR: false }
+  }
+}
