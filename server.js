1bcfee2976da8492aef321019a65f7a72b2e466f
rktel <rakinakuy@hotmail.com>
2019-05-07 15:34:15 -0500

    Anti Percy 2

--- server.js
+++ server.js
@@ -8,9 +8,17 @@ const server = net.createServer((c) => {
   c.on('data', (data) => {
     console.log('Received:', data.toString());
   })
+  c.on('error', (...arg) => {
+    console.log('Client Error:', ...arg)
+    setTimeout(ns => {
+      server.listen(3001, () => {
+        console.log('server bound, port 3001');
+      });
+    }, 2500)
+  })
 });
 server.on('error', (err) => {
-  console.log('Error: ', err)
+  console.log('Server Error: ', err)
 });
 server.listen(3001, () => {
   console.log('server bound, port 3001');
